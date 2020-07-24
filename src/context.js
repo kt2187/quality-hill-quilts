ead9a8a1b89318c55a008762416e0d3280df99d5
Katie Barriere <kbarriere@gmail.com>
2020-07-24 13:03:17 -0400

    Add basic layout for cart UI

--- src/context.js
+++ src/context.js
@@ -7,9 +7,12 @@ class ProductProvider extends Component {
     state = {
         products: [],
         detailProduct: detailProduct,
-        cart: [],
+        cart: storeProducts,
         modalOpen: false,
-        modalProduct: detailProduct
+        modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0
     }
 
     componentDidMount() {
@@ -64,7 +67,23 @@ class ProductProvider extends Component {
     closeModal = () => {
         this.setState(() => {
             return { modalOpen: false }
-        })
+        });
+    };
+
+    increment = (id) => {
+        console.log('This is increment method');
+    }
+
+    decrement = (id) => {
+        console.log('This is decrement method');
+    }
+
+    removeItem = (id) => {
+        console.log('Item removed');
+    }
+
+    clearCart = () => {
+        console.log('Cart was emptied');
     }
 
     render() {
@@ -75,7 +94,11 @@ class ProductProvider extends Component {
                     handleDetail: this.handleDetail,
                     addToCart: this.addToCart,
                     openModal: this.openModal,
-                    closeModal: this.closeModal
+                    closeModal: this.closeModal,
+                    increment: this.increment,
+                    decrement: this.decrement,
+                    removeItem: this.removeItem,
+                    clearCart: this.clearCart
                 }}>
                 {this.props.children}
             </ProductContext.Provider>
