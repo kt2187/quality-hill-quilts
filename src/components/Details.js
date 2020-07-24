d99f49ec06ca14d7b69c525ec376bcbbe4aed1c9
Katie Barriere <kbarriere@gmail.com>
2020-07-24 15:49:03 -0400

    Remove unused variable in Details.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { ProductConsumer } from '../context';
 import { Link } from 'react-router-dom';
 import { ButtonContainer } from './Button';
-import { detailProduct } from '../data';
 
 export default class Details extends Component {
     render() {
