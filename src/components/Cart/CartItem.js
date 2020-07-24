226a8b91522901c13ec2b137361991244ce5f5a1
Katie Barriere <kbarriere@gmail.com>
2020-07-24 13:37:14 -0400

    Add button to clear cart, add cart total calculations

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -4,7 +4,7 @@ export default function CartItem({ item, value }) {
     const { id, title, img, price, total, count } = item;
     const { increment, decrement, removeItem } = value;
     return (
-        <div className="row my-1 text-capitalize text-center">
+        <div className="row my-2 text-capitalize text-center">
             <div className="col-10 mx-auto col-lg-2">
                 <img
                     src={img}
@@ -24,10 +24,21 @@ export default function CartItem({ item, value }) {
                 <div className="display-flex justify-content-center">
                     <div>
                         <span className="btn btn-black mx-1" onClick={() => decrement(id)}>-</span>
+                        <span className="btn btn-black mx-1">{count}</span>
+                        <span className="btn btn-black mx-1" onClick={() => increment(id)}>+
+                            </span>
                     </div>
                 </div>
             </div>
             {/* */}
+            <div className="col-10 mx-auto col-lg-2">
+                <div className="cart-icon" onClick={() => removeItem(id)}>
+                    <i className="fa fa-trash" />
+                </div>
+            </div>
+            <div className="col-10 mx-auto col-lg-2">
+                <strong>item total : $ {total}</strong>
+            </div>
         </div>
-    )
+    );
 }
--- src/context.js
+++ src/context.js
@@ -10,9 +10,9 @@ class ProductProvider extends Component {
         cart: storeProducts,
         modalOpen: false,
         modalProduct: detailProduct,
-        cartSubTotal: 0,
-        cartTax: 0,
-        cartTotal: 0
+        cartSubtotal: 10,
+        cartTax: 20,
+        cartTotal: 30
     }
 
     componentDidMount() {
